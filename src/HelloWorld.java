07ba3b89d72646c8f0ac14bca97ab0820f7c4bad
sunilhavannavar <49519931+sunilhavannavar@users.noreply.github.com>
2019-09-16 14:14:44 +0530

    adding method doSomeThing

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,14 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		HelloWorld obj = new HelloWorld();
+		obj.doSomeThing();
+		
+		
+	}
+	
+	void doSomeThing(){
+		System.out.println("Calling doSomeThing method");
 	}
 
 }
